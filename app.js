5d3d089c3ace28f8a23e0d67898d8ef23d7015ec
Sumit Patil <sumitpatil1134@gmail.com>
2026-03-13 22:38:03 +0530

    some changes

--- app.js
+++ app.js
@@ -56,9 +56,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // });
 const store=MongoStore.create({
   mongoUrl:dbUrl,
-  // crypto:{
-  //   secret:process.env.SECRET,
-  // },
+  crypto:{
+    secret:process.env.SECRET,
+  },
   touchAfter:24*3600,
 });
 
