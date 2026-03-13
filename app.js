c73c6a035cf0ed4c85482af7f07c2dc2000383f5
Sumit Patil <sumitpatil1134@gmail.com>
2026-03-13 23:17:51 +0530

    last commit

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ if(process.env.NODE_ENV!="production"){require('dotenv').config();
 
 const express = require("express");
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 
 const methodOverride = require("method-override");
