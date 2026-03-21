b450be75a10de50deaf7e733aa14115c2489b261
Sumit Patil <sumitpatil1134@gmail.com>
2026-03-22 00:39:07 +0530

    last

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const { validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
-const reviewController = require("../controllers/reviews.js");
+const reviewController = require("../controllers/review.js");
 
 
 //Post Route
